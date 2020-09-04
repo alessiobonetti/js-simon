830d95d0260ee655b10be7c8dde4ef744dfab747
Alessio Bonetti <alesine87@gmail.com>
2020-09-04 16:52:26 +0200

    b

--- js/script.js
+++ js/script.js
@@ -3,9 +3,44 @@ function numGenerator (min, max) {
 }
 // array vuoto
 var randomNumber = [];
+var userArrayNumbers = [];
 // push 5 numeri in array
 for(i=0; i<5; i++){
  randomNumber.push(numGenerator(1,100)) ;
 }
 
 // espongo i numeri
+function tempAlert(msg,duration)
+{
+ var el = document.createElement("div");
+ el.setAttribute("style","position:absolute;top:40%;left:20%;background-color:white;");
+ el.innerHTML = msg;
+ setTimeout(function(){
+  el.parentNode.removeChild(el);
+ },duration);
+ document.body.appendChild(el);
+}
+
+tempAlert(randomNumber,5000);
+
+setTimeout(
+  function(){
+    for(i=0; i<5; i++) {
+      var userNumber = parseInt(prompt("Inserisci 1 dei 5 numeri che hai visto prima"));
+      userArrayNumbers.push(userNumber);
+    }
+  },8000);
+
+
+console.log(randomNumber);
+console.log(userArrayNumbers);
+
+// RISULTATO
+var equalNumbers = [];
+
+for (i=0; i<5; i++) {
+  if (randomNumber[i] == userArrayNumbers[i]){
+    equalNumbers.push(userArrayNumbers[i]);
+  }
+}
+console.log(equalNumbers)
